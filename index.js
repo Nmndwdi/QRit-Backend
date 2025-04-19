0ec6eb3c461946e08195bafbeb384870100970b8
naman <Nmndwdi1001@gmail.com>
2025-04-19 17:12:08 +0530

    .env added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const itemRouter=require('./routes/item');
 
 // INIT
 // const databaseURL = process.env.DB_URL;
-const databaseURL = "mongodb+srv://naman_dwivedi:Nmndwdi1001@cluster0.ed9wjt6.mongodb.net/?retryWrites=true&w=majority";
+const databaseURL = process.env.DB_URL;
 const PORT = process.env.PORT || 4000;
 const app=express();
 const DB = databaseURL;
@@ -18,7 +18,7 @@ const DB = databaseURL;
 app.options('*', cors())
 app.use(
     cors ({
-        origin:'*',
+        origin:'https://qrit.vercel.app',
         credential:true,
         preflightContinue:true,
     })
